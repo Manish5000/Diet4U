0c15c58b9dfcfafcff8dd2c0fccf4752fbaa43d4
Manish Parihar <pariharmanish5000@gmail.com>
2022-06-14 15:46:45 +0530

    diet images almost done

--- lib/Screens/aboutus.dart
+++ lib/Screens/aboutus.dart
@@ -23,47 +23,54 @@ class _AboutusState extends State<Aboutus> {
         padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Text('About Us',style: GoogleFonts.urbanist(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 30),)),
-              SizedBox(height: MediaQuery.of(context).size.height*0.070,),
-              Container(
-                 decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: leftButton
-                    ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Text('This is fitness and diet based application which will help you to keep fit and healthy.This application can be downloaded on mobile phones quite easily. The aim of this application is to make your lifestyle healthier by suggesting you right food intake, water intake and workout pattern. This application provide the function of BMI(Body Mass Index) calculator where the user can calculate their BMI by just inserting the weight, height and age.',
-                    textAlign: TextAlign.center, 
-                    style: GoogleFonts.urbanist(color: Colors.black,fontSize: 17,)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Container(
-                      width: 290,
-                      decoration: BoxDecoration(
+          children: [                          
+              Center(
+                child: Container(
+                   decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: Colors.white
+                        color: leftButton
                       ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
                 child: Column(
                   children: [
-                    Text('Developers',textAlign: TextAlign.center, style: GoogleFonts.urbanist(color: Colors.black,fontSize: 18,fontWeight: FontWeight.normal)),
-                    SizedBox(height: 20,),
-                    Text('Manish Parihar\nMD Shahjada',textAlign: TextAlign.start, style: GoogleFonts.urbanist(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold)),
-                    
-                  ],
-                ),
-                
-              )
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: 
+                      Column(
+                        children: [
+                          Text('About Us',style: GoogleFonts.urbanist(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 30),),
+                          SizedBox(height: MediaQuery.of(context).size.height*0.060,),
+                          Text('This is fitness and diet based application which will help you to keep fit and healthy.This application can be downloaded on mobile phones quite easily. The aim of this application is to make your lifestyle healthier by suggesting you right food intake, water intake and workout pattern. This application provide the function of BMI(Body Mass Index) calculator where the user can calculate their BMI by just inserting the weight, height and age.',
+                          textAlign: TextAlign.center, 
+                          style: GoogleFonts.urbanist(color: Colors.black,fontSize: 17,)),
+                        ],
+                      ),
                     ),
-                  )
-                ],
-              )
+                    SizedBox(height: MediaQuery.of(context).size.height*0.185,),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Container(
+                        width: 290,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white
+                        ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text('Developers',textAlign: TextAlign.center, style: GoogleFonts.urbanist(color: Colors.black,fontSize: 18,fontWeight: FontWeight.normal)),
+                      SizedBox(height: 20,),
+                      Text('Manish Parihar\nMD Shahjada',textAlign: TextAlign.start, style: GoogleFonts.urbanist(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold)),
+                      
+                    ],
+                  ),
+                  
+                )
+                      ),
+                    )
+                  ],
+                )
+                ),
               )
           ],
         ),
