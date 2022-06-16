86fe043584c9d7ed281131dfab555c0330b324c5
Madhacky <Mdshahjada747@gmail.com>
2022-06-16 23:15:54 +0530

    d

--- lib/Screens/SingupandSignin/chooseaccount.dart
+++ lib/Screens/SingupandSignin/chooseaccount.dart
@@ -22,71 +22,68 @@ class _ChooseAccountState extends State<ChooseAccount> {
   
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false ,
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-        ),
-        body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            return Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left:20,top: 40) ,
-                    child: Heading2(text: "Let's you in"),
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.height*0.150,),
-                  Buttonarea(text: 'Continue with E-mail',icon: Icons.mail,c: Colors.black,func: (){
-                    Navigator.push(context,MaterialPageRoute(builder: (context) =>  Loginwithmail()));
-                  },),
-                  SizedBox(height: 10,),
-                  Buttonarea(text: 'Conitnue with google', 
-                  icon: FontAwesomeIcons.google,
-                  c: Colors.black,
-                  iconsize: 20,
-                  func: (){
-                    final provider = Provider.of<GoogleSignInProvider>(context,listen: false);
-                    provider.googleLogin();
-                  },),
-                  SizedBox(height: MediaQuery.of(context).size.height*0.140,),
-                  Row(mainAxisAlignment: MainAxisAlignment.center,
-                  
-                    children: [
-                      Expanded(
-                        child: Container(
-                          height: 1,
-                          decoration: BoxDecoration(color: Colors.black),
-                        ),
-                      ),
-                      SizedBox(width: 5,),
-                      Text('Or'),
-                      SizedBox(width: 5,),
-                      Expanded(
-                        child: Container(height: 1,
-                        decoration: BoxDecoration(color: Colors.black),),
-                      ),
-                  ],),
-                  SizedBox(height: MediaQuery.of(context).size.height*0.150,),
-                  Button1(name: 'Signup', color1: rightbutton, func: (){
-                    Navigator.push(context, MaterialPageRoute(builder: ((context) => Signupwithmail()))); 
-                  })
-        
-                  
-                ],
-              ),
-            );
-          }
-        ),
-        
       ),
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:20,top: 40) ,
+                  child: Heading2(text: "Let's you in"),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height*0.150,),
+                Buttonarea(text: 'Continue with E-mail',icon: Icons.mail,c: Colors.black,func: (){
+                  Navigator.push(context,MaterialPageRoute(builder: (context) =>  Loginwithmail()));
+                },),
+                SizedBox(height: 10,),
+                Buttonarea(text: 'Conitnue with google', 
+                icon: FontAwesomeIcons.google,
+                c: Colors.black,
+                iconsize: 20,
+                func: (){
+                  final provider = Provider.of<GoogleSignInProvider>(context,listen: false);
+                  provider.googleLogin();
+                },),
+                SizedBox(height: MediaQuery.of(context).size.height*0.140,),
+                Row(mainAxisAlignment: MainAxisAlignment.center,
+                
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        decoration: BoxDecoration(color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(width: 5,),
+                    Text('Or'),
+                    SizedBox(width: 5,),
+                    Expanded(
+                      child: Container(height: 1,
+                      decoration: BoxDecoration(color: Colors.black),),
+                    ),
+                ],),
+                SizedBox(height: MediaQuery.of(context).size.height*0.150,),
+                Button1(name: 'Signup', color1: rightbutton, func: (){
+                  Navigator.push(context, MaterialPageRoute(builder: ((context) => Signupwithmail()))); 
+                })
+      
+                
+              ],
+            ),
+          );
+        }
+      ),
+      
     );
   }
 }
@@ -99,9 +96,9 @@ class Buttonarea extends StatefulWidget {
   final Color? c;
   final double? iconsize;
   final VoidCallback? func;
-bool? isemail;
+
   
-   Buttonarea({ @required this.text, @required this.icon,this.func, this.c,this.iconsize,this.isemail});
+   Buttonarea({ @required this.text, @required this.icon,this.func, this.c,this.iconsize,});
 
   @override
   State<Buttonarea> createState() => _ButtonareaState();
@@ -111,7 +108,7 @@ class _ButtonareaState extends State<Buttonarea> {
      final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return widget.isemail == true? 
+    return 
     
      ElevatedButton(
       
@@ -132,23 +129,6 @@ class _ButtonareaState extends State<Buttonarea> {
           Text(widget.text!,style: GoogleFonts.urbanist(fontSize: 14,color: Colors.black,fontWeight: FontWeight.bold))
         ],
       ),
-    ) : ElevatedButton(
-      
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        fixedSize: Size(MediaQuery.of(context).size.width*0.88, MediaQuery.of(context).size.height*0.070),
-        elevation: 5,
-        primary: Colors.white
-      ),
-      onPressed: widget.func,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(widget.icon,color: widget.c,size: widget.iconsize,),
-          SizedBox(width: 10,),
-          Text(widget.text!,style: GoogleFonts.urbanist(fontSize: 14,color: Colors.black,fontWeight: FontWeight.bold))
-        ],
-      ),
-    );
+    ) ;
   }
 }
